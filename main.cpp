5aea52540f9169c47df67d2798f23069608b084f
pauliendoms <paulien.doms@student.thomasmore.be>
2021-10-31 17:47:14 +0100

    menu functie gemaakt

--- main.cpp
+++ main.cpp
@@ -2,11 +2,34 @@
 using namespace std;
 
 void menu();
+void stock();
+void klanten();
+void facturatie();
 
 int main() {
 
 }
 
 void menu() {
+    int keuze;
+
     cout << "Wat wil je doen?" << endl;
+    cout << "1. Stock" << endl;
+    cout << "2. Klanten" << endl;
+    cout << "3. Facturatie" << endl;
+
+    cin >> keuze;
+
+    switch(keuze) {
+        case 1:
+            stock();
+            break;
+        case 2:
+            klanten();
+            break;
+        case 3:
+            facturatie();
+            break;
+    }
 }
+
